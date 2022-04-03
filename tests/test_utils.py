5a358908dbeab9caaf469a0910f437abba7a65a9
Chaitanya Joshi <chaitanya@brandeis.edu>
2022-04-03 10:08:56 -0400

    Updated tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -37,9 +37,13 @@ class TestUtils(unittest.TestCase):
     def test_denoise(self):
         pass
     
-    @skip("This test still needs to be written.")
     def test_add_noise(self):
-        pass
+
+        a = np.random.default_rng().standard_normal(tuple([100, 100]))
+        b = ut.add_noise(a.copy(), noise_strength=0.01)
+        noise = b - a
+        self.assertIsNone(assert_allclose(noise.mean(), 0, atol=0.02))
+        self.assertIsNone(assert_allclose(noise.std(), 0.01, rtol=0.02))
 
     @skip("This test still needs to be written.")
     def test_compute_Q(self):
@@ -86,30 +90,42 @@ class TestUtils(unittest.TestCase):
         # Expect the value to be +- pi/2, since they are equivalent for a nematic field
         self.assertAlmostEqual(abs(x[5,4]), 0.5 * np.pi)
 
-    def test__circular_shifts(self):
-        
-        csexp = [(1, 2, 3, 0), (2, 3, 0, 1), (3, 0, 1, 2), (0, 1, 2, 3)]
-        for i,cs in enumerate(ut._circular_shifts(range(4))):
-            self.assertEqual(cs,csexp[i])
-
-    @skip("This test still needs to be written.")
-    def test_set_boundary(self):
-        pass
-
-    @skip("This test still needs to be written.")
-    def test_set_boundary_region(self):
-        pass
-
     def test_count_NaNs(self):
         a = np.zeros([5,5])
         a[1,2] = np.nan 
         a[2,3] = np.nan
         self.assertEqual(ut.count_NaNs(a),2)
 
-    @skip("This test still needs to be written.")
     def test_get_random_sample(self):
-        pass
 
+        a = np.zeros([64,64,64])
+        shp = a.shape
+        num_points = 5
+        box_size = tuple([15,15,15])
+        diff_order = 2
+        points, views = ut.get_random_sample(shp, num_points, box_size, diff_order, seed=1)
+        self.assertEqual(len(points),5)
+        for _, view in views.items():
+            # The returned size is slightly larger than 15 so that we
+            # can ignore the 1 edge point in all directions to
+            # accomodate the 2nd order derivatives
+            self.assertEqual(a[view].shape, tuple([17,17,17])) 
+    
+    # Same as above but with diff_order 3
+    def test_get_random_sample3(self):
+
+        a = np.zeros([64,64,64])
+        shp = a.shape
+        num_points = 5
+        box_size = tuple([15,15,15])
+        diff_order = 3
+        points, views = ut.get_random_sample(shp, num_points, box_size, diff_order, seed=5)
+        self.assertEqual(len(points),5)
+        for _, view in views.items():
+            # The returned size is slightly larger than 15 so that we
+            # can ignore the 2 edges point in all directions to
+            # accomodate the 3rd order derivatives
+            self.assertEqual(a[view].shape, tuple([19,19,19])) 
 
 if __name__ == "__main__":
     unittest.main()
